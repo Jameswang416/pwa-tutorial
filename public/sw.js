f6c9b976ef5e4faeb1bd5a5dedd43a7ff4b3c24b
Jameswang416 <46651219+Jameswang416@users.noreply.github.com>
2022-04-03 08:49:54 -0400

    v

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
   );
-  console.log(workbox.registerRoute);
+  console.log(workbox);
  
  /*  import {
     pageCache,
