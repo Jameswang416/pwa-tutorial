df165b3cec2723a2f3bf0d37bbff1cb2e81741a9
Jameswang416 <46651219+Jameswang416@users.noreply.github.com>
2022-04-03 08:42:01 -0400

    c

--- public/sw.js
+++ public/sw.js
@@ -24,16 +24,16 @@ importScripts(
     // Check to see if the request's destination is style for an image
     ({ request }) => request.destination === 'image',
     // Use a Cache First caching strategy
-    new workbox.CacheFirst({
+    new workbox.registerRoute.CacheFirst({
       // Put all cached files in a cache named 'images'
       cacheName: 'images1',
       plugins: [
         // Ensure that only requests that result in a 200 status are cached
-        new workbox.CacheableResponsePlugin({
+        new workbox.registerRoute.CacheFirst.CacheableResponsePlugin({
           statuses: [200],
         }),
         // Don't cache more than 50 items, and expire them after 30 days
-        new workbox.ExpirationPlugin({
+        new workbox.registerRoute.CacheFirst.ExpirationPlugin({
           maxEntries: 50,
           maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
         }),
